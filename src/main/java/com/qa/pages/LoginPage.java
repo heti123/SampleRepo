1c9e0d84568d13f5d92671ad048afa63f882c2cf
Hetal <hetalsonani22@gmail.com>
2019-09-11 21:45:13 -0400

    added search page code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,8 +1,13 @@
 package com.qa.pages;
 
+
+
 public class LoginPage {
 	public void loginPage() {
 		System.out.println("Login page Method");
+	}
+	public void loginSet() {
+		System.out.println("Login Set");
 	}
 
 }
